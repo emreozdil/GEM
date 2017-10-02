928c2fcdf3e74f55fb5e23c04280614a4c01aa42
Emre Özdil <emre.ozdil@inomera.com>
2017-10-02 17:25:40 +0300

    Initial Commit

---
+++ GEM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GEM
+//
+//  Created by Emre Özdil on 02/10/2017.
+//  Copyright © 2017 Emre Özdil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
