defbbe46f7a63e0735ebe36c1dded429c7733e7a
Emre Özdil <emre.ozdil@inomera.com>
2017-10-09 18:19:26 +0300

    Add podfiles

---
+++ Pods/Target Support Files/Pods-GEM/Pods-GEM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GEM : NSObject
+@end
+@implementation PodsDummy_Pods_GEM
+@end
---
+++ Pods/Target Support Files/ProjectOxfordFace/ProjectOxfordFace-umbrella.h
@@ -0,0 +1,47 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MPOAccessories.h"
+#import "MPOAddPersistedFaceResult.h"
+#import "MPOBlur.h"
+#import "MPOCandidate.h"
+#import "MPOCreatePersonResult.h"
+#import "MPOExposure.h"
+#import "MPOFace.h"
+#import "MPOFaceAttributes.h"
+#import "MPOFaceEmotion.h"
+#import "MPOFaceFeatureCoordinate.h"
+#import "MPOFaceHeadPose.h"
+#import "MPOFaceLandmarks.h"
+#import "MPOFaceList.h"
+#import "MPOFaceListMetadata.h"
+#import "MPOFaceMetadata.h"
+#import "MPOFaceRectangle.h"
+#import "MPOFaceSDK.h"
+#import "MPOFaceServiceClient.h"
+#import "MPOFacialHair.h"
+#import "MPOGroupResult.h"
+#import "MPOHair.h"
+#import "MPOIdentifyResult.h"
+#import "MPOMakeup.h"
+#import "MPONoise.h"
+#import "MPOOcclusion.h"
+#import "MPOPerson.h"
+#import "MPOPersonFace.h"
+#import "MPOPersonGroup.h"
+#import "MPOSimilarFace.h"
+#import "MPOTrainingStatus.h"
+#import "MPOVerifyResult.h"
+
+FOUNDATION_EXPORT double ProjectOxfordFaceVersionNumber;
+FOUNDATION_EXPORT const unsigned char ProjectOxfordFaceVersionString[];
+
